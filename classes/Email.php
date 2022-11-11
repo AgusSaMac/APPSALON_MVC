601bd82e1c1c76198b4d4c693304ed2df788cab8
Agustin Sanchez Macal <agustin11089@gmail.com>
2022-11-11 17:55:55 -0600

    editing queries

--- classes/Email.php
+++ classes/Email.php
@@ -37,7 +37,7 @@ class Email {
 
         $contenido = "<html>";
         $contenido = "<p><strong>Hola ". $this->nombre . "</strong>. Has creado tu cuenta en AppSalon, solo debes confirmarla presionando el siguiente enlace </p>";
-        $contenido .= "<p>Presiona aqui: <a href='http://localhost:3000/confirmar-cuenta?token=" . $this->token . "'>Confirmar Cuenta</a></p>";
+        $contenido .= "<p>Presiona aqui: <a href='http://appsalon.agustinsanchez.dev/confirmar-cuenta?token=" . $this->token . "'>Confirmar Cuenta</a></p>";
         $contenido .= "<p>si tu no solicitaste esta cuenta, puedes ignorar el mensaje.</p>";
         $contenido .= "</html>";
         $mail->Body = $contenido;
